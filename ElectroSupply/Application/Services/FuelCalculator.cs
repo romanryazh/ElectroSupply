02ffd67d673f50854cd3caa83111a55eadc263b8
Roman Ryazh <romanryazh@yandex.ru>
2025-09-17 00:59:34 +0300

    Добавлен сервис расчёта заказа для электроснабжения

--- ElectroSupply/Application/Services/FuelCalculator.cs
+++ ElectroSupply/Application/Services/FuelCalculator.cs
@@ -1,6 +1,22 @@
+using ElectroSupply.Domain.Interfaces;
+using ElectroSupply.Domain.ValueObjects;
+
 namespace ElectroSupply.Application.Services;
 
-public class FuelCalculator
+public class FuelCalculator : IFuelCalculator
 {
+    private readonly IFuelCalculationStrategy _strategy;
+
+    public FuelCalculator(IFuelCalculationStrategy strategy)
+    {
+        ArgumentNullException.ThrowIfNull(strategy);
+            
+        _strategy = strategy;
+    }
     
+    public (IReadOnlyCollection<IGenerator>, Fuel totalFuel) CalculateRequiredFuel
+        (Power requiredPower, Days days, IReadOnlyCollection<IGenerator> generators)
+    {
+        return _strategy.Calculate(requiredPower, days, generators);
+    }
 }
---
+++ ElectroSupply/Domain/Interfaces/IFuelCalculationStrategy.cs
@@ -0,0 +1,9 @@
+﻿using ElectroSupply.Domain.ValueObjects;
+
+namespace ElectroSupply.Domain.Interfaces;
+
+public interface IFuelCalculationStrategy
+{
+    public (IReadOnlyCollection<IGenerator>, Fuel totalFuel) Calculate
+        (Power requiredPower, Days days, IReadOnlyCollection<IGenerator> generators);
+}
